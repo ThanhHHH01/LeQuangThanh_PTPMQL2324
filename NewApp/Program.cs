48b521c32d223993c6ba09c5af3f0f0d17ed5939
ThanhHHH01 <112218811+ThanhHHH01@users.noreply.github.com>
2023-08-16 11:10:46 +0700

    break

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -2,11 +2,10 @@
 {
     private static void Main(string[] args)
     {
-       int a = 1;
-       do
-       {
-        System.Console.WriteLine("Vongf lặp thứ {0}",a);
-        a++;
-       }while(a <10);
+        for(int i = 0; i < 10; i++)
+        {
+            if(i == 5) break;
+            System.Console.WriteLine("Vòng lặp thứ {0}", i);
+        }
     }
 }
