0c9ab6cbf69479d451ebb08786d133e991f2bd1b
ThanhHHH01 <112218811+ThanhHHH01@users.noreply.github.com>
2023-08-16 10:37:56 +0700

    cautrucrenhanhifelse

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -2,8 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        string str = "123";
-        int a = Convert.ToInt32(str);
-        System.Console.WriteLine("a = " + a);
+        float a=10;
+        if(a >= 8){
+            System.Console.WriteLine("Hoc sinh hoc luc gioi");
+        }else if(a >= 6.5){
+            System.Console.WriteLine("Hoc sinh hoc luc kha");
+        }else if(a >= 5){
+            System.Console.WriteLine("Hoc sinh hoc luc trung binh");
+        }else if(a >= 3.5){
+            System.Console.WriteLine("Hoc sinh hoc luc yeu");
+        }else{
+            System.Console.WriteLine("Hoc sinh hoc luc kem");
+        }
     }
 }
