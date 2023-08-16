04a948913627e4b296939c6fd84a4f5dece1009f
ThanhHHH01 <112218811+ThanhHHH01@users.noreply.github.com>
2023-08-16 10:48:08 +0700

    while

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -2,18 +2,11 @@
 {
     private static void Main(string[] args)
     {
-       int day = 2;
-       switch(day)
+       int a = 1;
+       while(a < 10)
        {
-        case 1 : System.Console.WriteLine("Chủ nhật"); break;
-        case 2 : System.Console.WriteLine("Thứ 2"); break;
-        case 3 : System.Console.WriteLine("Thứ 3"); break;
-        case 4 : System.Console.WriteLine("Thứ 4"); break;
-        case 5 : System.Console.WriteLine("Thứ 5"); break;
-        case 6 : System.Console.WriteLine("Thứ 6"); break;
-        case 7 : System.Console.WriteLine("Thứ 7"); break;
-        default :
-            System.Console.WriteLine("khôngg phải ngayf trong tuần"); break;
+            System.Controle.WriteLine("Vòng lặp thứ {0},a");
+            a++;
        }
     }
 }
