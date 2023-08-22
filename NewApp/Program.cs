0d4fbb7fc2bfdc5be6708951648f66bacbf0fc42
ThanhHHH01 <112218811+ThanhHHH01@users.noreply.github.com>
2023-08-22 09:48:08 +0700

    Program

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,10 +1,13 @@
 ﻿using NewApp.Models;
-
 public class Program
 {
     private static void Main(string[] args)
     {
         Person ps1 = new Person();
         Person ps2 = new Person();
+        ps1.FullName = "Nguyen Van a";
+        ps1.Address = "Ha Noi";
+        ps1.Age = 18;
+        ps1.Display();
     }
 }
